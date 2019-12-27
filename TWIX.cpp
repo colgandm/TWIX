ebe4911b386d1505a0206168bbc888d1aaaed591
Daniel <colgandm@tcd.ie>
2019-12-27 15:58:33 +0000

    Add project files.

---
+++ TWIX.cpp
@@ -0,0 +1,8 @@
+// TWIX.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include "game.h"
+
+int main(int argc, char* args[]){   
+    Game game;
+    return 0;
+}
---
+++ player.h
@@ -0,0 +1,14 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "animatedsprite.h"
+
+class Graphics;
+
+class Player {
+public:
+	Player();
+	~Player();
+private:
+};
+#endif
